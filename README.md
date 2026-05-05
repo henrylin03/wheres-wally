7f5f1e3c6de797b7321f9a8f65588415c6d66c71
Henry <83106787+henrylin03@users.noreply.github.com>
2026-05-05 15:56:48 +1000

    Merge pull request #1 from henrylin03/chore/setup-vite-react-ts-mantine-vitest-rtl
    chore: setup codebase

---
+++ README.md
@@ -0,0 +1,3 @@
+# Where's Wally
+
+Help us find Wally and his friends
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+const App = () => <MantineProvider>hello</MantineProvider>;
+
+export default App;
