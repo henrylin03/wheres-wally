ffe143147afc686a93759df8df4b44498988a267
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-05 18:02:25 +1000

    feat(Header): add header with icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import Header from "./components/Header";
 
-const App = () => <MantineProvider>hello</MantineProvider>;
+const App = () => (
+  <MantineProvider>
+    <Header />
+  </MantineProvider>
+);
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+
+const Header = () => (
+  <header className={styles.header}>
+    <a href="/">
+      <img
+        src="/logo-text-only.webp"
+        alt="Logo for Where's Wally"
+        width="144px"
+      />
+    </a>
+  </header>
+);
+
+export default Header;
