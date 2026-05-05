8088600748a3e69c520eb3d09a1df055579598ee
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-05 15:39:48 +1000

    chore: setup mantine
    ui library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
-const App = () => <>hello</>;
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+const App = () => <MantineProvider>hello</MantineProvider>;
 
 export default App;
