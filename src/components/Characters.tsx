4fc15f28cec7d57b84d6625e741631aa53e63218
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-05-11 15:02:49 +1000

    feat(Characters): avatars reduce in size in the top row of characters needing to be found, on narrower screens

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,4 +1,5 @@
 import { Group, Tooltip } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { CHARACTERS, type CharacterName } from "../mockData";
 import { capitaliseFirstLetter } from "../utils";
 import CharacterAvatar from "./CharacterAvatar";
@@ -8,19 +9,28 @@ interface Props {
   charactersFound: CharacterName[];
 }
 
-const Characters = ({ characterNames, charactersFound }: Props) => (
-  <Group gap="4rem" mx="auto" mt="xl" justify="center">
-    {characterNames.map((name) => (
-      <Tooltip key={name} label={capitaliseFirstLetter(name)} withArrow>
-        <CharacterAvatar
-          name={name}
-          img={CHARACTERS[name]}
-          size="xl"
-          isFound={charactersFound.includes(name)}
-        />
-      </Tooltip>
-    ))}
-  </Group>
-);
+const Characters = ({ characterNames, charactersFound }: Props) => {
+  const isNarrowScreen = useMediaQuery("(max-width: 495px)");
+
+  return (
+    <Group
+      gap={isNarrowScreen ? "md" : "4rem"}
+      mx="auto"
+      mt="xl"
+      justify="center"
+    >
+      {characterNames.map((name) => (
+        <Tooltip key={name} label={capitaliseFirstLetter(name)} withArrow>
+          <CharacterAvatar
+            name={name}
+            img={CHARACTERS[name]}
+            size={isNarrowScreen ? "lg" : "xl"}
+            isFound={charactersFound.includes(name)}
+          />
+        </Tooltip>
+      ))}
+    </Group>
+  );
+};
 
 export default Characters;
